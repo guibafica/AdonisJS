a0cb6fa1808becda69c71c6f5436a23e5898c490
guibafica <gb.engcomp@hotmail.com>
2020-10-08 10:09:38 -0300

    ADD: Internacionalizaçao

--- app/Validators/Task.js
+++ app/Validators/Task.js
@@ -1,11 +1,17 @@
 'use strict'
 
+const Antl = use('Antl')
+
 class Task {
   get rules () {
     return {
       title: 'required',
       due_date: 'date'
     }
+  }
+
+  get messages () {
+    return Antl.list('validation')
   }
 }
 
