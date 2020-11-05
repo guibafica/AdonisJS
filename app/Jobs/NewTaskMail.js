58cbd0cbad8285266ac4d1022f077b5c717f7b74
guibafica <gb.engcomp@hotmail.com>
2020-11-05 09:31:56 -0300

    ADD: Fila com Redis

---
+++ app/Jobs/NewTaskMail.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+
+class NewTaskMail {
+  static get concurrency () {
+    return 1
+  }
+
+  static get key () {
+    return 'NewTaskMail-job'
+  }
+
+  async handle ({ username, title, file, email }) {
+    console.log(`Job: ${NewTaskMail.key}`)
+
+    await Mail.send(
+      ['emails.new_task'],
+      { username, title, hasAttachment: !!file },
+      message => {
+        message
+          .to(email)
+          .from('gb.engcomp@hotmail.com', 'Guilherme | AngeluzTI')
+          .subject('Nova tarefa para você')
+
+        if (file) {
+          message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+            filename: file.name
+          })
+        }
+      }
+    )
+  }
+}
+
+module.exports = NewTaskMail
