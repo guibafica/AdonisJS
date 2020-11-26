a519a086d34546f3d7e7d059f0a7ed82d62f277b
guibafica <gb.engcomp@hotmail.com>
2020-11-26 14:34:59 -0300

    ADD: Salvando relacionamentos

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -5,8 +5,12 @@ const User = use('App/Models/User')
 class UserController {
   async store ({ request }) {
     const data = request.only(['username', 'email', 'password'])
+    const address = request.input('address')
+    // o input retorna apenas uma informação do body da requisição
 
     const user = await User.create(data)
+
+    await user.addresses().createMany(address)
 
     return user
   }
